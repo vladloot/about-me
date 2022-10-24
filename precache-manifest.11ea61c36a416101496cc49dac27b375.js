8f1dd6a264db264611bcc45ad9fd084becc7fb11
Agitobl <vlad.agito@gmail.com>
2022-10-25 02:02:57 +0300

    Updates

---
+++ precache-manifest.11ea61c36a416101496cc49dac27b375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce95f82cb5ee8ff1d7206fc9abdcf81d",
+    "url": "/about-me/index.html"
+  },
+  {
+    "revision": "5e27ec607fc46c9ff3d2",
+    "url": "/about-me/static/css/main.243d1680.chunk.css"
+  },
+  {
+    "revision": "feb0cfddc229046d293d",
+    "url": "/about-me/static/js/2.4c711095.chunk.js"
+  },
+  {
+    "revision": "db414a237edd910d64d9dd0ca73de7ec",
+    "url": "/about-me/static/js/2.4c711095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e27ec607fc46c9ff3d2",
+    "url": "/about-me/static/js/main.7e9b0388.chunk.js"
+  },
+  {
+    "revision": "bf7a2b35aca46a649b02",
+    "url": "/about-me/static/js/runtime-main.4a9ef140.js"
+  }
+]);
