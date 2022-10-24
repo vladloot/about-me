473e3eda8750c3e38a0812aac721ae65cdf80c4f
Agitobl <vlad.agito@gmail.com>
2022-10-25 00:36:30 +0300

    update library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { render } from "react-snapshot";
+import ReactDOM from 'react-dom';
 import { App } from "./app/App";
 import "./index.css";
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
